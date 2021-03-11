40453ae8d3584771eee5dcffa05f68340b22f0ee
Luiz Francisco Martins Bentes <luizfrancisco2000@gmail.com>
2021-03-10 23:07:39 -0400

    Initial Commit

---
+++ src/main/java/com/ifam/ss/evasao/Models/Link.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ifam.ss.evasao.Models;
+
+/**
+ *
+ * @author luizf
+ */
+public class Link {
+    //Classe de Permissão de acesso a links administrativos
+    
+}
