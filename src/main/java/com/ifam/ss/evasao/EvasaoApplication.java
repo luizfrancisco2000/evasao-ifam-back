923c599c4d794fc813fa5d2e930e2cc95a9182cf
Luiz Francisco Martins Bentes <luizfrancisco2000@gmail.com>
2021-03-10 23:02:18 -0400

    Initial Commit

---
+++ src/main/java/com/ifam/ss/evasao/EvasaoApplication.java
@@ -0,0 +1,13 @@
+package com.ifam.ss.evasao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvasaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvasaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifam/ss/evasao/EvasaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifam.ss.evasao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvasaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
